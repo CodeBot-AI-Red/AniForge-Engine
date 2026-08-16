a738dd45818b3e412a9539dd833b5ca4c8694a83
CodeBot-AI-Red <zestfrosty@gmail.com>
2026-08-16 10:17:30 -0300

    fix: adiciona plugin Kotlin ao build.gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android") version "1.9.20"
 }
 
 android {
@@ -25,11 +26,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    buildFeatures {
-        aidl = false
-        renderScript = false
-        resValues = false
-        shaders = false
+    kotlinOptions {
+        jvmTarget = "11"
     }
 }
 
@@ -37,17 +35,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.11.0")
-    
-    // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
-    
-    // Fragment
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-    
-    // RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-    
-    // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
 }
